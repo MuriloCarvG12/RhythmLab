a26056ff6ebafc54eed0f9d20d5fd425a1bc853b
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2024-12-13 21:00:17 -0300

    style: adiciona novas estilizações de layout para a pagina home add: adiciona paleta de cores

--- RhythmLab/src/pages/pageHome.tsx
+++ RhythmLab/src/pages/pageHome.tsx
@@ -4,15 +4,18 @@ export default function PageHome()
 {
     return (
         
-        <div className='container'>
-            <h1>Hello there! i am page home</h1>
+        <div className='container' id='container'>
+            <div className='header'>
+                <div><h1>Hello there! i am page home</h1></div>
+            </div>
+            
             <div className='row-item-container'>
                 <button className='button'> </button>
                 <button className='button'> </button>
                 <button className='button'> </button>
             </div>
 
-            <h1> Future Text</h1>
+            <footer><div className='footer'> </div></footer>
         </div>
         
       )
