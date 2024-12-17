769cf980e155231c06ad775fe6707ae969083d23
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2024-12-17 18:03:05 -0300

    feature: adiciona funcionalidades de rotas aos botões da pagina home

--- RhythmLab/src/pages/pageHome.tsx
+++ RhythmLab/src/pages/pageHome.tsx
@@ -1,4 +1,5 @@
 import '.././index.css'
+import { Link } from 'react-router'
 
 export default function PageHome()
 {
@@ -11,9 +12,9 @@ export default function PageHome()
             </div>
             
             <div className='row-item-container'>
-                <button className='button'> </button>
-                <button className='button'> </button>
-                <button className='button'> </button>
+                <Link to='/metronome'><button className='button'> </button></Link>
+                <Link to='/practice'><button className='button'> </button></Link>
+                <Link to='/theory'><button className='button'> </button></Link>
             </div>
 
             <footer><div className='footer'> </div></footer>
