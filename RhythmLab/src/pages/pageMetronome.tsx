e8f978f1098313f24c406e02c0726ae4f497c360
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2024-12-23 17:34:43 -0300

    Feature: adiciona a pagina metronome Feature: adiciona slider a pagina permitindo ao usuario selecionar o valor que deseja

--- RhythmLab/src/pages/pageMetronome.tsx
+++ RhythmLab/src/pages/pageMetronome.tsx
@@ -1,8 +1,29 @@
+import '.././index.css'
+import { useState } from 'react'
+
 export default function PageMetronome()
 {
+    const [time, set_time] = useState(0)
+
+    const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>)=> {
+      set_time(Number(event.target.value));
+    };
+
     return (
         <>
-          <h1>Hello there i am page Metronome!</h1>
+          <div className='container' id='container'>
+
+          <div className='header'>
+                <div><h1>Hello there! i am page home</h1></div>
+                <img src='../.././public/Favicon.png' className='favicon'></img>
+            </div>
+
+            <input type='range' min={0} max={360} value={time} onChange={handleSliderChange} />
+            current time = {time}
+
+
+
+          </div>
         </>
       )
 
