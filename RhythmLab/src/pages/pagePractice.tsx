250bb19192f4eb6f274a5b85f324945ccb22fe39
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-01-06 09:38:35 -0300

    Feature: o texto em cima da div de ritmo agora mostra a dificuldade selecionada!

--- RhythmLab/src/pages/pagePractice.tsx
+++ RhythmLab/src/pages/pagePractice.tsx
@@ -7,6 +7,28 @@ export default function PagePractice()
 {
     const [picked_difficulty, set_picked_difficulty] = useState(0)
 
+    let displayText;
+
+    
+    switch (picked_difficulty) {
+
+      case 1:
+        displayText = 'Pratica - Facil';
+        break;
+
+      case 2:
+        displayText = 'Pratica - Medio';
+        break;
+
+      case 3:
+        displayText = 'Pratica - Dificil';
+        break;
+
+      default:
+        displayText = 'This wasnt supossed to happen!';
+        break;
+
+    }
   
 
     return (
@@ -20,44 +42,60 @@ export default function PagePractice()
                                 <img src='../.././public/Favicon.png' className='favicon'></img>
                           </div>
 
+                    {/** renderiza a opcao de selecionar dificuldade */}
 
-                          <div style={{backgroundColor: '#F0544F', textAlign: 'center', borderBlockStyle: 'solid', borderBlockWidth: 3, borderBlockColor: '#F0544F', width: '100%', paddingRight: 6}}>
-                                <h1> Selecione uma dificuldade </h1>
-                          </div>
+                    {picked_difficulty == 0 
+                    ?
+                    <>
+                                <div style={{backgroundColor: '#F0544F', textAlign: 'center', borderBlockStyle: 'solid', borderBlockWidth: 3, borderBlockColor: '#F0544F', width: '100%', paddingRight: 6}}>
+                                            <h1> Selecione uma dificuldade </h1>
+                                </div>
 
-                    <div className= 'metronome-body' style={{width: '100%', height: '100%', paddingTop: 50}}>
+                                        <div className= 'metronome-body' style={{width: '100%', height: '100%', paddingTop: 50}}>
+                                          
+                                              <div className='row-item-container'>
 
-                      
+                                                  <Button 
+                                                    handleClick={() => set_picked_difficulty(1)}
+                                                    text={"Facil"}
+                                                    bgColor="#5bff42"
+                                                    
+                                                  />
+                                                  <Button
+                                                    handleClick={() => set_picked_difficulty(2)}
+                                                    text={"Medio"}
+                                                    bgColor="#ffdc42"
 
+                                                  />
+                                                  <Button
+                                                  handleClick={() => set_picked_difficulty(3)}
+                                                  text={"Dificil"}
+                                                  bgColor="#ff5542"
+                                                  />
 
-                          <div className='row-item-container'>
+                                        </div>
 
-                              <Button 
-                                handleClick={() => set_picked_difficulty(1)}
-                                text={"Facil"}
-                                bgColor="#5bff42"
+                                </div>
                                 
-                              />
-                              <Button
-                                handleClick={() => set_picked_difficulty(2)}
-                                text={"Medio"}
-                                bgColor="#ffdc42"
+                    </>
 
-                              />
-                              <Button
-                               handleClick={() => set_picked_difficulty(3)}
-                               text={"Dificil"}
-                               bgColor="#ff5542"
-                              />
+                    :
 
-                          </div>
+                    <>
+                    </>
 
-                    </div>
+
+                    }
 
                     { picked_difficulty > 0 
 
                       ?
+
                       <>
+                              <div style={{backgroundColor: '#F0544F', textAlign: 'center', borderBlockStyle: 'solid', borderBlockWidth: 3, borderBlockColor: '#F0544F', width: '100%', paddingRight: 6}}>
+                                            <h1> {displayText} </h1>
+                              </div>
+
                               <div className="Sheet_Space">
 
                                   <div 
@@ -77,22 +115,21 @@ export default function PagePractice()
 
                                     <div style={{display: 'flex',width: "90%", alignContent: 'center', height: 50, justifyContent: 'center', paddingTop: 50}}>
 
-                                    <Button
-                                          handleClick={() => set_picked_difficulty(0)}
-                                          text={"Retornar"}
-                                    />
+                                            <Button
+                                                  handleClick={() => set_picked_difficulty(0)}
+                                                  text={"Retornar"}
+                                            />
 
                                     </div>
-                              </div>
 
-                          
-
-
-
+                              </div>                     
                         </>
-                      :
+
+                      : //else we just dont render anything.... obvious comment for better readability
+
                         <>
                         </>
+
                     }
 
             </div>
