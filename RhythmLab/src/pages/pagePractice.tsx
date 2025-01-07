ef9dbe91a4056545138486cfb17bf0659aa4c2bb
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-01-07 09:42:34 -0300

    Feature: adiciona suport a inputs the teclado para pagina practice

--- RhythmLab/src/pages/pagePractice.tsx
+++ RhythmLab/src/pages/pagePractice.tsx
@@ -1,15 +1,14 @@
 import Button from "../components/button"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 
 
 export default function PagePractice()
 {
-    const [picked_difficulty, set_picked_difficulty] = useState(0)
+    // difficulty related things 
 
-    let displayText;
-
-    
+    const [picked_difficulty, set_picked_difficulty] = useState(0) // stores the difficulty picked
+    let displayText; // stores the text based on the difficulty
     switch (picked_difficulty) {
 
       case 1:
@@ -27,9 +26,35 @@ export default function PagePractice()
       default:
         displayText = 'This wasnt supossed to happen!';
         break;
-
     }
-  
+
+    // input detection things
+
+    const [key_pressed, set_key_pressed ] = useState(null)   // useed to store input detection 
+    const [toggle_effect, set_toggle_effect] = useState(false) // used to create a visual effect everytime a key is pressed
+
+    function handle_key_pressed(event)
+    {
+        set_key_pressed(event.key)
+        console.log(`The key pressed was - ${key_pressed}`)
+
+    } 
+   
+    function clean_key_pressed()
+    {
+      set_key_pressed(null)
+    }
+
+    useEffect(() => {
+      document.addEventListener('keydown', handle_key_pressed);  // checks if keys are being held down if so it calls our function to tell us which one is being pressed
+      document.addEventListener('keyup', clean_key_pressed); // checks if keys are not being held down, if so it clears our key_pressed usestate
+
+      return () => {
+        document.removeEventListener('keydown', handle_key_pressed);
+        document.removeEventListener('keyup', clean_key_pressed);
+      };
+      }, []);
+
 
     return (
         <>
@@ -97,23 +122,53 @@ export default function PagePractice()
                               </div>
 
                               <div className="Sheet_Space">
-
+                                  {/*** below me is a div that represents the space of the sheet area */}
                                   <div 
-                                    style=
-                                    {{
-                                      marginTop: 40,
-                                      marginBottom: 40,
-                                      backgroundColor: 'aqua',
-                                      height: "100%",
-                                      width: "90%",
-                                    }}>
+                                  style=
+                                  {{position: 'relative', 
+                                    height: "100%",
+                                    width: "90%",
+                                    alignItems: 'center'
+                                  }}>
 
+
+
+                                      <div 
+                                        style=
+                                        {{
+                                          marginTop: 40,
+                                          marginBottom: 40,
+                                          backgroundColor: 'aqua',
+                                          height: 200,
+                                          width: 200,
+                                          position: 'relative',
+                                          zIndex: 1
+                                        }}>    
+
+
+                                              <div 
+                                              style=
+                                              {{
+                                                marginTop: 40,
+                                                marginBottom: 40,
+                                                backgroundColor: 'red',
+                                                height: 100,
+                                                width: 100,
+                                                position: 'relative',
+                                                zIndex: 0
+                                              }}>                              
+                                            </div>                          
+                                      </div>
 
                                       
 
-                                  </div>
 
-                                    <div style={{display: 'flex',width: "90%", alignContent: 'center', height: 50, justifyContent: 'center', paddingTop: 50}}>
+                                    </div>
+
+
+
+
+                                    <div style={{display: 'flex',width: "90%", alignContent: 'center', height: 50, justifyContent: 'center'}}>
 
                                             <Button
                                                   handleClick={() => set_picked_difficulty(0)}
