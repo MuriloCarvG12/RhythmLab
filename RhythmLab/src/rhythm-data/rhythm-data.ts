e3389ece433cc92f709777f83c8965ac68ad6687
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-01-09 10:05:58 -0300

    Feature: a barra de progresso agora é manipulada dinamicamente pela funcao startRhythm

--- RhythmLab/src/rhythm-data/rhythm-data.ts
+++ RhythmLab/src/rhythm-data/rhythm-data.ts
@@ -1,8 +1,8 @@
 export const rhythm = [
-    { type: 'note1', duration: 'half', time: 5000},
-    { type: 'note2', duration: 'half', time: 5000},
-    { type: 'rest3', duration: 'half', time: 5000},
-    { type: 'note4', duration: 'half', time: 5000},
-    { type: 'note5', duration: 'half', time: 5000},
-    { type: 'note6', duration: 'half', time: 5000},
+    { type: 'note1', duration: 'half', time: 2000},
+    { type: 'note2', duration: 'half', time: 2000},
+    { type: 'rest3', duration: 'half', time: 2000},
+    { type: 'note4', duration: 'half', time: 2000},
+    { type: 'note5', duration: 'half', time: 2000},
+    { type: 'note6', duration: 'half', time: 2000},
   ];
