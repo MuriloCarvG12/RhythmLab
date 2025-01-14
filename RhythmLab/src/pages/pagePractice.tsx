452fd9ce967152af21d9ef085824a208b7967789
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-01-14 09:23:03 -0300

    Feature: A funcao startRhythm() agora é capaz de determinar o timing esperado para o input do usuario de acordo com cada nota

--- RhythmLab/src/pages/pagePractice.tsx
+++ RhythmLab/src/pages/pagePractice.tsx
@@ -101,6 +101,7 @@ export default function PagePractice()
     {
       const audio = new Audio("../.././public/sounds/metronome.mp3");
       const start_time = Date.now() //this represents the current time the rhythm has started it will be used as a base to calculate all other future timings
+      console.log(`the initial time is ${start_time}`)
       set_isPlaying(true)
 
       const playNextNote =  (noteIndex: number) =>  // this is a recursive function that takes the current index and checks if the current index is the last one! if not we will keep navigating through the whole array
@@ -111,11 +112,12 @@ export default function PagePractice()
           set_isPlaying(false); // stop playback when we reach the end of the rhythm array.
           return;
         }
+
         // if we are not in the last note then
          set_current_note(noteIndex) // Update the current note index.
          
-        
-  
+        const expected_time = start_time + Number(rhythm.slice(0,  noteIndex).reduce((sum, note) => sum + note.time, 0))
+        console.log(`the expected time is ${expected_time}`)
 
         // Schedule the next note based on the duration of the current note.
         timeoutRef.current = window.setTimeout(() => {
