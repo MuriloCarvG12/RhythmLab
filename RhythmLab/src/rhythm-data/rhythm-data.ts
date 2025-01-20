5b88940b16e2b6c36232f84d0e4d76eba4bf8ac2
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-01-20 14:27:57 -0300

    Style: altera a cor das notas tipo 'Whole Note'

--- RhythmLab/src/rhythm-data/rhythm-data.ts
+++ RhythmLab/src/rhythm-data/rhythm-data.ts
@@ -3,18 +3,18 @@
 
 
 const rhythm_easy = [
-    { type: 'Whole Note', color: '#FF5733', time: 1000}, // color orange
+    { type: 'Whole Note', color: '#700aff', time: 1000}, // color purple
     { type: 'Half Note', color: '#ff12ef', time: 500}, // color pink
   ]
 
 const rhythm_medium = [
-  { type: 'Whole Note', color: '#FF5733', time: 1000}, // color orange
+  { type: 'Whole Note', color: '#700aff', time: 1000}, // color purple
   { type: 'Half Note', color: '#ff12ef', time: 500}, // color pink
   { type: 'Quarter Note', color: '#5733FF', time: 250} // color blue 
 ]
 
 const rhythm_hard = [
-  { type: 'Whole Note', color: '#FF5733', time: 1000}, // color orange
+  { type: 'Whole Note', color: '#700aff', time: 1000}, // color purple
   { type: 'Half Note', color: '#ff12ef', time: 500}, // color pink
   { type: 'Quarter Note', color: '#5733FF', time: 250}, // color blue  
   { type: 'Eighth Note', color: '#FFC300', time: 125}, // color  yellow 
