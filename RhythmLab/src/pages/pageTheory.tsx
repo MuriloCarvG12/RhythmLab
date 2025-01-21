22c8ed9a22311fbcf1fffa176e9aa49976cc1e42
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-01-21 09:14:46 -0300

    Feature: novo layout para a pagina Theory

--- RhythmLab/src/pages/pageTheory.tsx
+++ RhythmLab/src/pages/pageTheory.tsx
@@ -1,18 +1,40 @@
 import Header from "../components/header"
-
+import Sheet_space_header from "../components/sheet_space_header"
 
 export default function PageTheory()
 {
     return (
         <>
-          <div className="container" style={{height: '100vh', width: '100vh'}}>      
-
-              <div style={{height: '60vh', width: '100%'}}>
+          <div className="container" style={{height: 'auto', width: '100vh'}}>      
+              
+              <div className="header-size"  style={{height: 400, width: '100%'}}>
                 <Header/>
               </div>
 
-              <div className="metronome-body" style={{width: '80%'}}>
-            
+              <div className='Paragraph'>
+                <p> A  </p>
+                <p> Test </p>
+                <p> Test </p>
+                
+
+              </div>
+              
+              
+              <div style={{width: "80.3%", marginTop: 40}}>
+                  <Sheet_space_header
+                  displayText="Teoria Para Ritmos"
+                  timeDifferenceColor="#F0544F"
+                  />
+              </div>
+
+
+              <div className="metronome-body" style={{width: '80%', height: 'auto', display: 'flex', flexDirection: 'column', paddingTop: 40}}>
+                    <div className="row-item-container"></div>
+                    <div className="row-item-container"></div>
+                    <div className="row-item-container"></div>
+                    <div className="row-item-container"></div>
+                    <div className="row-item-container"></div>
+                    <div className="row-item-container"></div>
               </div>
 
           </div>
