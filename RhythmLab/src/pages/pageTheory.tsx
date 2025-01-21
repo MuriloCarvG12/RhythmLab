d8a12ee4981e29dde0b7dfa836966a652b9bc761
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-01-21 08:46:29 -0300

    Inicializa a pagina page Theory

--- RhythmLab/src/pages/pageTheory.tsx
+++ RhythmLab/src/pages/pageTheory.tsx
@@ -1,8 +1,21 @@
+import Header from "../components/header"
+
+
 export default function PageTheory()
 {
     return (
         <>
-          <h1>Hello there!i am page theory</h1>
+          <div className="container" style={{height: '100vh', width: '100vh'}}>      
+
+              <div style={{height: '60vh', width: '100%'}}>
+                <Header/>
+              </div>
+
+              <div className="metronome-body" style={{width: '80%'}}>
+            
+              </div>
+
+          </div>
         </>
       )
 
