d5040215701727e97fd3379707e6d4163d3ffc84
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-01-28 10:09:46 -0300

    Fix: corrige erros de caminho para a imagem da nota Style: adiciona uma estilizacao para arrendondar a borda do elemento geratodo no main_game.txt

--- RhythmLab/src/rhythm-data/rhythm-data.ts
+++ RhythmLab/src/rhythm-data/rhythm-data.ts
@@ -17,7 +17,7 @@ const rhythm_hard = [
   { type: 'Whole Note', color: '#700aff', time: 1000, image:'../.././public/note_durations_symbols/whole_note.png'}, // color purple
   { type: 'Half Note', color: '#ff12ef', time: 500, image:'../.././public/note_durations_symbols/half_note.png'}, // color pink
   { type: 'Quarter Note', color: '#5733FF', time: 250, image:'../.././public/note_durations_symbols/quarter_note.png'}, // color blue  
-  { type: 'Eighth Note', color: '#FFC300', time: 125, image:'../.././public/note_durations_symbols/eighth_note.png'}, // color  yellow 
+  { type: 'Eighth Note', color: '#FFC300', time: 125, image:'../.././public/note_durations_symbols/eight_note.png'}, // color  yellow 
 ]
 
 export default {rhythm_easy, rhythm_medium, rhythm_hard}
